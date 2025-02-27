854e70701377adc068494d85b9c5e50a45e6d55d
Son Ahyun <dkgus731@naver.com>
2025-02-27 21:12:14 +0900

    🔥 데모데이터 삭제

--- front/src/pages/Home/api/fetchTodayScheduleData.ts
+++ front/src/pages/Home/api/fetchTodayScheduleData.ts
@@ -17,6 +17,8 @@ export const fetchTodayScheduleData = async (): Promise<TodayScheduleDTO[]> => {
       }
     );
 
+    console.log("API 응답 데이터:", response.data);
+
     // 응답 데이터에서 `data` 속성만 반환
     if (!Array.isArray(response.data.data)) {
       throw new Error("API 응답 데이터 형식이 배열이 아닙니다.");
@@ -28,87 +30,3 @@ export const fetchTodayScheduleData = async (): Promise<TodayScheduleDTO[]> => {
     return [];
   }
 };
-
-// export const fetchTodayScheduleData = async (): Promise<TodayScheduleDTO[]> => {
-//   try {
-//     // Mock 데이터 반환
-//     return [
-//       {
-//         roomNumber: 104,
-//         startTime: "10:00",
-//         endTime: "11:45",
-//         type: "강의",
-//         title: "웹서비스설계및실습",
-//         professor: "김교수",
-//       },
-//       {
-//         roomNumber: 103,
-//         startTime: "10:00",
-//         endTime: "11:45",
-//         type: "강의",
-//         title: "웹서비스설계및실습",
-//         professor: "김교수",
-//       },
-//       {
-//         roomNumber: 103,
-//         startTime: "11:00",
-//         endTime: "11:45",
-//         type: "강의",
-//         title: "자료구조",
-//         professor: "김교수",
-//       },
-//       {
-//         roomNumber: 103,
-//         startTime: "13:00",
-//         endTime: "15:45",
-//         type: "예약",
-//         title: "동아리 개강총회",
-//         professor: "김교수",
-//       },
-//       {
-//         roomNumber: 103,
-//         startTime: "10:00",
-//         endTime: "11:45",
-//         type: "예약",
-//         title: "세미나",
-//         professor: "김교수",
-//       },
-
-//       {
-//         roomNumber: 103,
-//         startTime: "10:00",
-//         endTime: "11:45",
-//         type: "강의",
-//         title: "웹서비스설계및실습",
-//         professor: "김교수",
-//       },
-//       {
-//         roomNumber: 104,
-//         startTime: "20:30",
-//         endTime: "23:00",
-//         type: "강의",
-//         title: "인공지능 개론",
-//         professor: "최교수",
-//       },
-//       {
-//         roomNumber: 205,
-//         startTime: "20:30",
-//         endTime: "23:00",
-//         type: "강의",
-//         title: "인공지능 개론",
-//         professor: "최교수",
-//       },
-//       {
-//         roomNumber: 715,
-//         startTime: "20:30",
-//         endTime: "23:00",
-//         type: "강의",
-//         title: "인공지능 개론",
-//         professor: "최교수",
-//       },
-//     ];
-//   } catch (error) {
-//     console.error("Error fetching schedule data:", error);
-//     return [];
-//   }
-// };
