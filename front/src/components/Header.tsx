ddb7560f5315cd5a7ab94229d06fdc96f04f4013
Son Ahyun <dkgus731@naver.com>
2025-02-27 20:55:43 +0900

    ✨ 로그아웃시 토큰 삭제

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,17 +1,24 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    sessionStorage.removeItem("accessToken"); // 토큰 삭제
+    navigate("/login"); // 로그인 페이지로 이동
+  };
+
   return (
     <header className="fixed top-0 left-1/2 transform -translate-x-1/2 w-full max-w-[600px] bg-opacity-80 bg-white backdrop-blur-lg shadow-md p-4 flex justify-between items-center z-50">
       <h1 className="text-lg md:text-xl font-bold text-gray-900">
         강의실 예약 시스템
       </h1>
-      <Link
-        to="/login"
+      <button
+        onClick={handleLogout}
         className="text-gray-700 hover:text-blue-500 font-semibold text-sm"
       >
         로그아웃
-      </Link>
+      </button>
     </header>
   );
 };
