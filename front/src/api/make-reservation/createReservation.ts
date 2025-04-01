8d8d028658650c4b88b8a10b38f014264affb454
Son Ahyun <dkgus731@naver.com>
2025-04-01 20:20:56 +0900

    ✨ 예약하기 api생성 및 dto 설정

---
+++ front/src/api/make-reservation/createReservation.ts
@@ -0,0 +1,39 @@
+import { createReservationDto } from "./dto/createReservationDto";
+
+export const createReservation = async (data: createReservationDto) => {
+  try {
+    const token = sessionStorage.getItem("accessToken"); // 세션스토리지에서 토큰을 가져옴
+    const studentId = sessionStorage.getItem("studentId"); // 세션스토리지에서 학생 ID를 가져옴
+
+    const reservationData = {
+      studentId, // 세션에서 가져온 studentId
+      date: data.date,
+      building: data.building,
+      room: data.room,
+      startTime: data.startTime,
+      endTime: data.endTime,
+      purpose: data.purpose,
+      professor: data.professor,
+      participantCount: data.participantCount,
+    };
+
+    const response = await fetch("/api/reservation-list", {
+      method: "POST", // POST 요청 사용
+      headers: {
+        "Content-Type": "application/json", // 요청 본문이 JSON 형식임을 명시
+        Authorization: `Bearer ${token}`, // Bearer 토큰 헤더에 포함
+      },
+      body: JSON.stringify(reservationData), // 예약 데이터를 요청 본문에 포함
+    });
+
+    if (!response.ok) {
+      throw new Error("데이터를 가져오는 데 실패했습니다.");
+    }
+
+    const responseData = await response.json(); // JSON 형태로 응답 데이터 파싱
+    return responseData;
+  } catch (error) {
+    console.error("Failed to fetch reservation notices:", error);
+    throw error; // 오류 발생 시 오류를 던짐
+  }
+};
---
+++ front/src/api/make-reservation/dto/createReservationDto.ts
@@ -0,0 +1,10 @@
+export interface createReservationDto {
+  date: string; // 예약 날짜
+  building: string; // 건물명
+  room: string; // 호실
+  startTime: string; // 시작 시간
+  endTime: string; // 종료 시간
+  purpose: string; // 예약 목적
+  professor: string; // 담당 교수명
+  participantCount: number; // 참여 인원 수
+}
