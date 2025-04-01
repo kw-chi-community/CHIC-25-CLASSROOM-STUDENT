d3c5971684b463378973a0f398cd197699738dbb
Son Ahyun <dkgus731@naver.com>
2025-04-01 15:53:00 +0900

    💄 헤더에 로고 추가

--- front/src/components/Header/MainHeader.tsx
+++ front/src/components/Header/MainHeader.tsx
@@ -12,9 +12,15 @@ const MainHeader = () => {
 
   return (
     <header className="fixed top-0 left-0 right-0 mx-auto w-full max-w-[600px] bg-opacity-50 bg-white backdrop-blur-lg p-4 flex justify-between items-center z-50">
-      <h1 className="text-lg md:text-xl font-bold text-gray-900">
-        강의실 예약 시스템
-      </h1>
+      <div className="flex justify-between items-center gap-2">
+        <img
+          src="/logo.svg"
+          alt="로고"
+          className="h-8 w-auto" // 크기 조정 (필요에 따라 수정)
+        />
+        <h1 className="text-lg md:text-xl font-medium ">강의실 예약 시스템</h1>
+      </div>
+
       <button
         onClick={handleLogout}
         aria-label="로그아웃"
