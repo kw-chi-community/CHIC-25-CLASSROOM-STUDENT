64864b3f33fb69752cd19dfd77ec7cd8b1808dc5
Son Ahyun <dkgus731@naver.com>
2025-04-08 22:48:21 +0900

    🔨 StudentSchema에서 버전키 제거

--- back/db/student.js
+++ back/db/student.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 const { studentDB } = require("./mongodb");
 
-const StudentSchema = new mongoose.Schema({
-  studentId: { type: String, unique: true, required: true },
-  email: { type: String, unique: true, required: true },
-  name: { type: String, required: true },
-  password: { type: String, required: true },
-  phoneNumber:{ type: String, required: true }
-});
+const StudentSchema = new mongoose.Schema(
+  {
+    studentId: { type: String, unique: true, required: true },
+    email: { type: String, unique: true, required: true },
+    name: { type: String, required: true },
+    password: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+  },
+  { versionKey: false }
+);
 
 module.exports = studentDB.model("Student", StudentSchema);
