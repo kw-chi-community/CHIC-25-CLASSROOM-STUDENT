04ed265c1a024f3f8fad451a95f9d78879591312
Son Ahyun <dkgus731@naver.com>
2025-04-15 19:48:06 +0900

    🐛 예약 상세 및 예약 목록 API를 GET에서 POST로 변경

--- back/routes/reservationList.js
+++ back/routes/reservationList.js
@@ -3,23 +3,25 @@ const router = express.Router();
 const Reservation = require("../db/reservation");
 
 // POST /api/reservation-list
-router.get("/reservation-list", async (req, res) => {
+router.post("/reservation-list", async (req, res) => {
   try {
     const { studentId } = req.body;
 
     if (!studentId) {
       return res.status(400).json({ message: "studentId가 필요합니다." });
     }
-    const reservations = await Reservation.find({ student_id: studentId }).populate("classroom_info_id");
+    const reservations = await Reservation.find({
+      student_id: studentId,
+    }).populate("classroom_info_id");
 
-    const result = reservations.map(r => ({
+    const result = reservations.map((r) => ({
       id: r._id,
       building: r.classroom_info_id?.building || null,
       room: r.classroom_info_id?.room || null,
       reserve_date: new Date(r.reserve_date).toISOString().slice(0, 10),
       reserve_start_time: r.reserve_start_time,
       reserve_end_time: r.reserve_end_time,
-      reservation_confirmed: r.reservation_confirmed
+      reservation_confirmed: r.reservation_confirmed,
     }));
 
     return res.status(200).json(result);
