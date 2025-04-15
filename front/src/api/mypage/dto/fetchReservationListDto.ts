fe5ef751016ce24e9e47781f95dc566e8fc8001a
Son Ahyun <dkgus731@naver.com>
2025-04-15 16:44:01 +0900

    🔨 fetchReservationListDto 인터페이스 수정 및 fetchReservationList 함수에서 타입 지정 추가

--- front/src/api/mypage/dto/fetchReservationListDto.ts
+++ front/src/api/mypage/dto/fetchReservationListDto.ts
@@ -3,6 +3,7 @@ export interface fetchReservationListDto {
   reserve_date: Date;
   reserve_start_time: string;
   reserve_end_time: string;
-  reserve_reason: string; // 예약 사유
-  reservation_confirmed: boolean; // 예약 확인 여부 (0: 예약 대기, 1: 예약 확정)
+  building: string;
+  room: string;
+  reservation_confirmed: boolean; // 예약 확인 여부 (0: 예약 반려, 1: 예약 확정)
 }
--- front/src/api/mypage/fetchReservationList.ts
+++ front/src/api/mypage/fetchReservationList.ts
@@ -1,3 +1,5 @@
+import { fetchReservationListDto } from "./dto/fetchReservationListDto";
+
 export const fetchReservationList = async () => {
   try {
     const token = sessionStorage.getItem("accessToken"); // 세션스토리지에서 토큰을 가져옴
@@ -16,7 +18,7 @@ export const fetchReservationList = async () => {
       throw new Error("데이터를 가져오는 데 실패했습니다.");
     }
 
-    const data = await response.json(); // JSON 형태로 응답 데이터 파싱
+    const data: fetchReservationListDto = await response.json(); // JSON 형태로 응답 데이터 파싱
     return data;
   } catch (error) {
     console.error("Failed to fetch reservation notices:", error);
