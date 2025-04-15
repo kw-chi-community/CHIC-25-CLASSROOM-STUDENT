edc9c923a98cc4de4047121840ae49b66f0d5a95
Son Ahyun <dkgus731@naver.com>
2025-04-15 15:38:23 +0900

    ✨ AlertPopup 컴포넌트 추가

---
+++ front/src/components/Popup/AlertPopup.tsx
@@ -0,0 +1,23 @@
+import { createPortal } from "react-dom";
+import Button from "../Button";
+
+interface AlertPopupPopupProps {
+  text: string;
+  onClose: () => void;
+}
+
+const AlertPopupPopup = ({ text, onClose }: AlertPopupPopupProps) => {
+  return createPortal(
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+      <div className="flex flex-col gap-5 bg-white w-full max-w-md p-6 rounded-lg relative shadow-lg">
+        <div className="flex flex-col items-center">
+          <p className="text-xl font-medium py-20">{text}</p>
+          <Button text={"확인"} onClick={onClose} isActive={true} />
+        </div>
+      </div>
+    </div>,
+    document.body
+  );
+};
+
+export default AlertPopupPopup;
