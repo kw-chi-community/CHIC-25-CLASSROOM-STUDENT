3e83f7d0a99418cdfd0d699a1c737366092866e6
Son Ahyun <dkgus731@naver.com>
2025-04-22 11:54:27 +0900

    🔧 fetchProfileDataDto에서 예약 상태 필드 제거 및 이메일, 전화번호 필드 추가

--- front/src/api/mypage/dto/fetchProfileDataDto.ts
+++ front/src/api/mypage/dto/fetchProfileDataDto.ts
@@ -1,10 +1,5 @@
 export interface fetchProfileDataDto {
   name: string;
-  reservation_status: {
-    roomNumber: number;
-    date: string;
-    startTime: string; // 시작 시간 (HH:mm)
-    endTime: string; // 종료 시간 (HH:mm)
-    title: string;
-  };
+  email: string;
+  phoneNumber: string;
 }
--- front/src/api/mypage/fetchProfileData.ts
+++ front/src/api/mypage/fetchProfileData.ts
@@ -6,22 +6,17 @@ export const fetchProfileData = async (
   studentId: string
 ): Promise<fetchProfileDataDto | null> => {
   try {
-    // const response = await axios.get<fetchProfileDataDto>(
-    //   `${import.meta.env.VITE_API_URL}/api/mypage`,
-    //   { params: { studentId } }
+    // const response = await axios.post<fetchProfileDataDto>(
+    //   `${import.meta.env.VITE_API_URL}/api/user-info`,
+    //   { studentId }
     // );
 
     // return response.data;
 
     const demoData: fetchProfileDataDto = {
       name: "홍길동",
-      reservation_status: {
-        roomNumber: 103,
-        date: "2025-03-15",
-        startTime: "09:00",
-        endTime: "10:30",
-        title: "팀 프로젝트 회의",
-      },
+      email: "dkgus731@naver.com",
+      phoneNumber: "010-3547-1458",
     };
     return demoData;
   } catch (error) {
