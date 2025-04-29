1b72c7901a338e2d81bc419ac7322dcfe8ec1cd8
정수진 <124863799+ds-sujin@users.noreply.github.com>
2025-04-29 18:31:15 +0900

    show-notice popup 구현 완료, isHidden이 False인것들

---
+++ back/db/noticePopup.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const { noticeDB } = require("./mongodb");
+
+const noticePopupSchema = new mongoose.Schema({
+  studentId: { type: String, required: true },
+  noticeId: { type: mongoose.Schema.Types.ObjectId, required: true }
+}, { versionKey: false });
+
+module.exports = noticeDB.model("NoticePopup", noticePopupSchema, "notice-popup");
--- back/server.js
+++ back/server.js
@@ -34,6 +34,7 @@ app.use("/api", require("./routes/deletereserve"));
 app.use("/api", require("./routes/getreserve"));
 app.use("/api", require("./routes/noticeList"));
 app.use("/api", require("./routes/noticeDetail"));
+app.use("/api", require("./routes/showPopup"));
 
 // 서버 실행
 const PORT = process.env.PORT || 4000;
