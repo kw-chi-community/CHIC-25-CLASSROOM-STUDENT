0da53d4fd26a9f6a59a4d70ba0ff94ca3cd6fb8f
Son Ahyun <dkgus731@naver.com>
2025-05-04 15:11:34 +0900

    ✨ fetchReservationNoticeDetailDto 인터페이스 추가

---
+++ front/src/api/notice/dto/fetchReservationNoticeDetailDto.ts
@@ -0,0 +1,7 @@
+export interface fetchReservationNoticeDetailDto {
+  id: string; // 공지 아이디
+  created_at: string; // 공지 작성 시간
+  type: boolean; // 공지 타입 (예: false: 고정, true: 팝업)
+  title: string; // 공지 제목
+  contents: string; // 공지 내용
+}
