cdc586f6f1f0ebf7195c01c9625145e891744f95
Son Ahyun <dkgus731@naver.com>
2025-05-04 15:11:29 +0900

    🔨 fetchReservationNoticesDto의 created_at 타입을 Date에서 string으로 수정

--- front/src/api/notice/dto/fetchReservationNoticesDto.ts
+++ front/src/api/notice/dto/fetchReservationNoticesDto.ts
@@ -1,6 +1,6 @@
 export interface fetchReservationNoticesDto {
   id: string; // 공지 아이디
-  created_at: Date; // 공지 작성 시간
+  created_at: string; // 공지 작성 시간
   type: boolean; // 공지 타입 (예: false: 고정, true: 팝업)
   title: string; // 공지 제목
 }
